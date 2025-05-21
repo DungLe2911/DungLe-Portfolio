ebf9e83ea3c88c1ed4ca1cebc27d02a45b515188
DungLe2911 <lehoangdung29111998@gmail.com>
2025-05-21 00:24:38 -0400

    starting the setup for email service

--- src/Page/Contact.js
+++ src/Page/Contact.js
@@ -2,16 +2,29 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "../Style/Contact.css"
 import { APIProvider, Map, AdvancedMarker, Pin, InfoWindow } from "@vis.gl/react-google-maps"
 import * as FaSolid from '@fortawesome/free-solid-svg-icons'
-import { useState } from "react";
-export default function Contact(){
+import { useEffect, useState } from "react";
+export default function Contact() {
     const position = {
-        lat:30.832,
+        lat: 30.832,
         lng: -83.279
     }
     const mapAPIKEY = process.env.REACT_APP_GOOGLE_MAPS_API_KEY;
     const mapID = process.env.REACT_APP_GOOGLE_MAPS_ID;
-    const [enableSubmit, setEnableSubmit] = useState(true);
-    return(
+    const [disabledSubmit, setDisabledSubmit] = useState(true);
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [message, setMessage] = useState("");
+
+    useEffect(() => {
+        const isValid = name.trim() !== "" && email.trim() !== "" && message.trim() !== "";
+        setDisabledSubmit(!isValid);
+    }, [name, email, message]);
+
+    const handleSubmit = () => {
+        alert('hehehehe')
+    }
+
+    return (
         <div className="pageContainer">
             <header>
                 <h2 className=" h2 articleTitle">
@@ -29,17 +42,15 @@ export default function Contact(){
             </section>
             <section className="contactForm">
                 <h3 className="h3 formTitle"> Contact Form</h3>
-                <form action="#" className="form">
-                    <div className="inputWrapper">
-                        <input type="text" name="fullname" className="formInput" placeholder="Full name" required />
-                        <input type="email" name="email" className="formInput" placeholder="Email address" required />
-                    </div>
-                    <textarea name="message" className="formInput" placeholder="Your Message" required  />
-                    <button className="formBtn" type="submit" disabled={enableSubmit}>
-                        <FontAwesomeIcon icon={FaSolid.faPaperPlane}/>
-                        <span>Send Message</span>
-                    </button>
-                </form>
+                <div className="inputWrapper">
+                    <input value={name} type="text" onChange={(e) => { setName(e.target.value) }} name="name" className="formInput" placeholder="Full name" required />
+                    <input value={email} type="email" onChange={(e) => { setEmail(e.target.value) }} name="email" className="formInput" placeholder="Email address" required />
+                </div>
+                <textarea value={message} name="message" onChange={(e) => { setMessage(e.target.value) }} className="formInput" placeholder="Your Message" required />
+                <button className={ disabledSubmit ? 'formBtn' : 'formBtn valid'} onClick={handleSubmit} type="submit" disabled={disabledSubmit}>
+                    <FontAwesomeIcon icon={FaSolid.faPaperPlane} />
+                    <span>Send Message</span>
+                </button>
             </section>
         </div>
     )

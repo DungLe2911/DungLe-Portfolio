9e2b9f46e9839b7cae790f25c0bd8d444751b30c
DungLe2911 <lehoangdung29111998@gmail.com>
2026-02-04 20:27:23 -0500

    Fixing bug unable to load PDF

--- src/Component/PDFPreview.js
+++ src/Component/PDFPreview.js
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 import "../Style/PDFPreview.css"
 
-pdfjs.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.10.111/pdf.worker.min.js';
+pdfjs.GlobalWorkerOptions.workerSrc = `${process.env.PUBLIC_URL}/pdf.worker.min.js`;
 
 const PDFPreview = (props) => {
   const [numPages, setNumPages] = useState();

cbb021f3c2d3b19c2a0d2811904e35fe7d8b81d9
DungLe2911 <lehoangdung29111998@gmail.com>
2026-04-29 23:18:46 -0400

    Fixing PDF Error

--- src/Component/DownloadBtn.js
+++ src/Component/DownloadBtn.js
@@ -1,9 +1,9 @@
 import '../Style/DownloadBtn.css'
-import resume from '../Asset/Dung_Le_Resume.pdf'
+// import resume from '../Asset/Dung_Le_Resume.pdf'
 export default function DownloadBtn() {
-    // const url = `${process.env.PUBLIC_URL}/Asset/Dung_Le_Resume.pdf`
+    const url = `${process.env.PUBLIC_URL}/assets/pdf/Dung_Le_Resume.pdf`
     return (
-        <a href={resume} download className="download-btn">
+        <a href={url} download className="download-btn">
             <button>Download Resume (PDF)</button>
         </a>
     )

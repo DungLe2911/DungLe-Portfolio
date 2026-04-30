ed39ee9d3be00e29bccd83994fa653aaf7eae428
DungLe2911 <lehoangdung29111998@gmail.com>
2026-04-29 23:14:24 -0400

    Fixing resume being white

--- src/Component/DownloadBtn.js
+++ src/Component/DownloadBtn.js
@@ -1,9 +1,9 @@
 import '../Style/DownloadBtn.css'
-// import resume from 'src/Asset/pdf/Dung Le Resume.pdf'
+import resume from '../Asset/Dung_Le_Resume.pdf'
 export default function DownloadBtn() {
-    const url = `${process.env.PUBLIC_URL}/assets/pdf/Dung_Le_Resume.pdf`
+    // const url = `${process.env.PUBLIC_URL}/Asset/Dung_Le_Resume.pdf`
     return (
-        <a href={url} download className="download-btn">
+        <a href={resume} download className="download-btn">
             <button>Download Resume (PDF)</button>
         </a>
     )
